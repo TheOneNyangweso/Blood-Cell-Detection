4524c31ab5b3a9e0170daf511bc4ed43ca319722
TheOneNyangweso <morurisammy5@gmail.com>
2024-01-11 13:20:20 +0300

    mino fixes at backend

--- backend/main.py
+++ backend/main.py
@@ -14,13 +14,35 @@ model = YOLO(
 async def predict(file: UploadFile = File(...)):
     # Reading the image file
     contents = await file.read()
-    nparr = np.fromstring(contents, np.uint8)
+    nparr = np.frombuffer(contents, np.uint8)
+    # nparr = np.fromstring(contents.decode("utf-8"), np.uint8)
     img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
+    print('ok')
     # Performing object detection
-    results = model(img)
-
-    # Extracting the class of the detected object
-    detected_class = results.xyxy[0][-1]
-
-    return {"class": detected_class}
+    print(model(img))  # Returns a list of Results objects
+#
+#    with open("results.txt", "w") as f:
+#        f.write(str(results))  # Replace content with your formatted information
+#        f.close()
+#    if results[0].probs is not None:
+#        detected_class = results[0].probs.argmax()
+#    else:
+#        detected_class = None  # Handle the case of no detections
+#
+#    # Map index to class name if available
+#    class_values = {
+#        1: 'Band_Neutrophils',
+#        2: 'Dyplastic_Cell',
+#        3: 'Giant_Platelet',
+#        4: 'Lymphoblast',
+#        5: 'Lymphocytes',
+#        6: 'Neutrophil',
+#        7: 'Promyelocytes',
+#        8: 'monocytes',
+#        9: 'myelocytes',
+#        10: 'not_cell',
+#        11: 'nucleated_RBC'
+#    }
+#
+#    return {"class_index": detected_class, "class_name": class_values}
